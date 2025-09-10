a8c05805da327823b87530da10a9c6ce82c582f3
Jack Cheng <chengkinhangjack@gmail.com>
2025-09-10 01:51:20 -0400

    Empty Files for q1

---
+++ Lab1/q1/part_c_python.py
@@ -0,0 +1,11 @@
+"""
+This is the Python Script used to answer question 1c
+"""
+
+def main():
+    print("Welcome to my answer for question 1c. Have a wonderful day")
+
+
+if __name__ == "__main__":
+    main()
+
