c25e773fad03c474a4d49e6cbaff8001ed1b0b40
Jack Cheng <chengkinhangjack@gmail.com>
2025-11-05 10:31:20 -0500

    Uploaded example code and write a comment outline for q1.

---
+++ Lab8/lab8_q1.py
@@ -0,0 +1,31 @@
+"""
+Code for Lab 8 Question 1
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print("----------START----------")
+    print("Hi! Welcome to Lab 8 Question 1!")
+
+    # Gauss-Seidel relaxation with replacement and overrelaxation.
+
+    # Step 1: Initialize the boundaries
+    # Look at the instructions and directions on Figure 1 of the instructions.
+
+    # Step 2: Looping...
+    # IF ITERATIONS < 100, or level of convergence not reached:
+    #   on each (x,y) value not on the boundary...
+    #       do the equation shown on the Computational background. T(x,y) = 1 + omega / 4 [....] - omega T(x,y)
+    
+    # Step 3: Output:
+    # plt.imshow(phi)
+    # plt.gray()
+    # plt.show()
+
+    print("----------END----------")
+
+
+if __name__ == "__main__":
+    main()
