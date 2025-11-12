c16e986b03688453eb8d19686b2c7c794e504383
Jack Cheng <chengkinhangjack@gmail.com>
2025-11-12 13:33:58 -0500

    initial commit of lab9q1. Created a small template for future.

---
+++ Lab9/lab9_q1.py
@@ -0,0 +1,19 @@
+"""
+Code for Lab 9 Question 1
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print("----------START----------")
+    print("Hi! Welcome to Lab 9 Question 1!")
+
+
+    # Code...
+
+    print("----------END----------")
+
+
+if __name__ == "__main__":
+    main()
---
+++ labx_qy.py
@@ -0,0 +1,19 @@
+"""
+Code for Lab X Question Y
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print("----------START----------")
+    print("Hi! Welcome to Lab X Question Y!")
+
+
+    # Code...
+
+    print("----------END----------")
+
+
+if __name__ == "__main__":
+    main()
